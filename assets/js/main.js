a817fd8151f4428a351b9524a2a28ba76c89769f
João Vitor Farias <joaofarias_16@hotmail.com>
2024-12-03 18:27:42 -0300

    stats pokemon modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,26 +8,24 @@ async function loadPokemonItens(offset, limit) {
   try {
     const pokemons = await pokeApi.getPokemon(offset, limit);
     let addToHtml = "";
-    pokemons.map((pokemon, index) => {
-      addToHtml += convertPokemonToLi(pokemon, offset + index);
+    pokemons.map((pokemon) => {
+      addToHtml += convertPokemonToLi(pokemon);
     });
     olPokemons.innerHTML += addToHtml;
 
-    // Adicionando event listeners aos itens de Pokémon
     document.querySelectorAll(".pokemon").forEach((pokemonItem) => {
       pokemonItem.addEventListener("click", (event) => {
+        console.log(event);
         const pokemonData = JSON.parse(event.currentTarget.dataset.pokemon);
         openModal(pokemonData);
       });
     });
-
-    console.log(pokemons);
   } catch (error) {
     console.error("Erro ao carregar os Pokémons:", error);
   }
 }
 
-function convertPokemonToLi(pokemon, index) {
+function convertPokemonToLi(pokemon) {
   return `
   <li class="pokemon ${pokemon.type}" data-pokemon='${JSON.stringify(pokemon)}'>
     <span class="number">#${pokemon.number}</span>
@@ -46,9 +44,8 @@ function convertPokemonToLi(pokemon, index) {
 
 function openModal(pokemon) {
   modalContainer.style.display = "flex";
-  modalContainer.className = "modal-container"; // Reseta as classes anteriores
-  modalContainer.classList.add(pokemon.type); // Adiciona a classe do tipo atual
-  // Supondo que você tenha algum conteúdo dinâmico para exibir no modal
+  modalContainer.className = "modal-container"; // Limpa as classes anteriores
+  modalContainer.classList.add(pokemon.type);
   modalContainer.innerHTML = `
     <div class="options">
       <button class="back-to-pokedex">⇤</button>
@@ -72,10 +69,7 @@ function openModal(pokemon) {
         <li>Height: ${pokemon.height || "N/A"}</li>
         <li>Weight: ${pokemon.weight || "N/A"}</li>
         <li>Abilities: ${pokemon.abilities ? pokemon.abilities.join(", ") : "N/A"}</li>
-        <li>Base Experience: ${pokemon.base_experience || "N/A"}</li>
-        <li>Species: ${pokemon.species || "N/A"}</li>
-        <li>Generation: ${pokemon.generation || "N/A"}</li>
-        <li>Evolution Line: ${pokemon.evolution_line ? pokemon.evolution_line.join(" -> ") : "N/A"}</li>
+        <li>Base Experience: ${pokemon.baseExperience || "N/A"}</li>
       </ol>
     </div>
   `;
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -20,11 +20,22 @@ pokeApi.getPokemonDetail = (results) => {
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
   const pokemon = new Pokemon();
+  console.log(pokeDetail)
   pokemon.number = pokeDetail.id;
   pokemon.name = pokeDetail.name;
   pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
   pokemon.type = pokeDetail.types[0].type.name;
   pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  pokemon.baseExperience = pokeDetail.base_experience;
+  pokemon.height = pokeDetail.height;
+  pokemon.weight = pokeDetail.weight;
+  pokemon.abilities = pokeDetail.abilities.map((ability) => ability.ability.name);
+  pokemon.stats = pokeDetail.stats.map((stat) => ({
+    name: stat.stat.name,
+    url: stat.stat.url,
+  }));
+
   pokemon.name = pokeDetail.name[0].toUpperCase() + pokeDetail.name.slice(1);
+  console.log(pokemon.stats)
   return pokemon;
 }
